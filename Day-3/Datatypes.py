c87bb2c81ef63572f136232c63a34a35dabd4bda
Dasari Ajay <ajaydasari304@gmail.com>
2026-05-28 17:53:37 +0530

    Add files via upload

---
+++ Day-3/Datatypes.py
@@ -0,0 +1,22 @@
+DATATYPES:
+    Numeric:
+        ------
+        int
+        float
+        complex
+        
+    Sequential datatype:
+        -------
+        string
+        list:collection of datatypes enclosed in a square brackets
+            properties:
+                allow duplicates,dynamic datatype,mutable,ordered,heterogenous
+        tuple:immutable,static,all same as list
+        
+    Mapping dattypes:
+        set:unordered,mutable,heterogenous,no duplicates
+        dict:key value pair
+        
+        bool
+        None
+        
