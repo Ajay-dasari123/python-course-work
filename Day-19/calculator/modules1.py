6e20ba93dd5e8fee906051e75745f5f3a87604bd
Dasari Ajay <ajaydasari304@gmail.com>
2026-06-20 12:56:20 +0530

    Add files via upload

---
+++ Day-19/calculator/modules1.py
@@ -0,0 +1,55 @@
+import logic
+print(logic.add(3,4))
+print(logic.sub(3,4))
+print(logic.mul(3,4))
+print(logic.div(3,4))
+print(logic.mod(3,4))
+print(logic.exp(3,4))
+
+
+'''OUTPUT:
+7
+-1
+12
+0.75
+3
+81
+'''
+
+import logic as lg
+print(lg.add(3,4))
+print(lg.sub(3,4))
+print(lg.mul(3,4))
+print(lg.div(3,4))
+print(lg.mod(3,4))
+print(lg.exp(3,4))
+
+
+'''OUTPUT:
+7
+-1
+12
+0.75
+3
+81
+'''
+
+from logic import add,sub,mul
+print(add(1,2))
+print(sub(1,2))
+print(mul(1,2))
+
+'''OUTPUT:
+3
+-1
+2
+'''
+
+
+from logic import *
+print(add(1,2))
+print(sub(1,2))
+print(mul(1,2))
+print(div(1,2))
+print(mod(1,2))
+print(exp(1,2))
---
+++ Day-19/modules.py
@@ -0,0 +1,7 @@
+import logic
+print(logic.add(3,4))
+print(logic.sub(3,4))
+print(logic.mul(3,4))
+print(logic.div(3,4))
+print(logic.mod(3,4))
+print(logic.exp(3,4))
